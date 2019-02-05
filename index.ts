e7730d1b3baccaf1fa7b244bc1c973a081e4cddf
ipcrm <mcadorette@gmail.com>
2019-02-05 12:53:23 -0500

    Adding tunnel examples

--- index.ts
+++ index.ts
@@ -14,40 +14,89 @@
  * limitations under the License.
  */
 
-import { Configuration } from "@atomist/automation-client";
+import {
+    ApolloGraphClientFactory,
+    AxiosHttpClient,
+    Configuration, configurationValue,
+    HttpClient,
+    HttpClientFactory,
+    WSWebSocketFactory,
+} from "@atomist/automation-client";
+import { SoftwareDeliveryMachineOptions } from "@atomist/sdm";
 import {
     ConfigureOptions,
     configureSdm,
 } from "@atomist/sdm-core";
+import axios, { AxiosRequestConfig } from "axios";
+// @ts-ignore
+import { buildAxiosFetch } from "axios-fetch";
+import * as tunneling from "tunnel";
+import * as WebSocket from "ws";
 import { machine } from "./lib/machine/machine";
 
-const machineOptions: ConfigureOptions = {
-    /**
-     * When your SDM requires configuration that is unique to it,
-     * you can list it here.
-     */
-    requiredConfigurationValues: [
-    ],
+function getProxyConfig(): tunneling.HttpsOverHttpOptions["proxy"] {
+    return configurationValue<tunneling.HttpsOverHttpOptions["proxy"]>("sdm.proxy");
 };
 
-/**
- * The starting point for building an SDM is here!
- */
-export const configuration: Configuration = {
-    /**
-     * To run in team mode, you'll need an Atomist workspace.
-     * To run in local mode, you don't. This will be ignored.
-     * See: https://docs.atomist.com/developer/architecture/#connect-your-sdm
-     */
-    workspaceIds: ["connect this SDM to your whole team with the Atomist service"],
+const machineOptions: ConfigureOptions = {
+    requiredConfigurationValues: [],
+};
+
+class ProxyAxiosHttpClientFactory implements HttpClientFactory {
+
+    public create(url?: string): HttpClient {
+        return new ProxyAxiosHttpClient();
+    }
+}
+
+function createAxiosRequestConfig(config: AxiosRequestConfig): AxiosRequestConfig {
+    const tunnel = tunneling.httpsOverHttp({
+        proxy: getProxyConfig(),
+    });
+
+    return {
+        ...config,
+        httpsAgent: tunnel,
+        proxy: false,
+    };
+}
+
+class ProxyAxiosHttpClient extends AxiosHttpClient {
+
+    protected configureOptions(config: AxiosRequestConfig): AxiosRequestConfig {
+        return createAxiosRequestConfig(config);
+    }
+}
+
+class ProxyWSWebSocketFactory extends WSWebSocketFactory {
+
+    protected configureOptions(options: WebSocket.ClientOptions): WebSocket.ClientOptions {
+        const tunnel = tunneling.httpsOverHttp({
+            proxy: getProxyConfig(),
+        });
+
+        return {
+            ...options,
+            agent: tunnel,
+        };
+    }
+}
+
+class ProxyApolloGraphClientFactory extends ApolloGraphClientFactory {
+
+    protected configure(config: Configuration): GlobalFetch["fetch"] {
+        return buildAxiosFetch(axios.create(createAxiosRequestConfig({})));
+    }
+}
+
+export const configuration: Configuration & Partial<SoftwareDeliveryMachineOptions> = {
     postProcessors: [
-        /**
-         * This is important setup! This defines the function that will be called
-         * to configure your SDM with everything that you want it to do.
-         *
-         * Click into the first argument (the "machine" function) to personalize
-         * your SDM.
-         */
         configureSdm(machine, machineOptions),
+        async (config: Configuration) => {
+            config.http.client.factory = new ProxyAxiosHttpClientFactory();
+            config.ws.client.factory = new ProxyWSWebSocketFactory();
+            config.graphql.client.factory = new ProxyApolloGraphClientFactory();
+            return config;
+        },
     ],
 };
